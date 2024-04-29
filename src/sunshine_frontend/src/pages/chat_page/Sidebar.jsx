13bc39757bb4ba547152949044902898216f6560
josetano2 <jose@Jose>
2024-04-29 11:28:10 +0700

    chat

--- src/sunshine_frontend/src/pages/chat_page/Sidebar.jsx
+++ src/sunshine_frontend/src/pages/chat_page/Sidebar.jsx
@@ -8,20 +8,23 @@ export default function Sidebar() {
   return (
     <>
       {/* The left nav */}
-      <div className="flex-col justify-center h-screen w-24 py-3 bg-gray-800">
-        <img className="w-16" src={logoImg} alt="" />
-        {/* icons */}
-        <div className="flex-col place-content-center justify-center mt-16 gap-5">
-          <SidebarIcon
-            icon={
-              <UserIcon className="w-10 text-cream-custom stroke-1" />
-            }
-          />
-          <SidebarIcon
-            icon={
-              <ChatBubbleOvalLeftEllipsisIcon className="w-10 text-cream-custom stroke-1" />
-            }
-          />
+      <div className="flex flex-col justify-between h-screen w-24 py-3 bg-gray-800">
+        <div>
+          <img className="w-16" src={logoImg} alt="" />
+          {/* icons */}
+          <div className="flex-col place-content-center justify-center mt-16 gap-5">
+            <SidebarIcon
+              icon={<UserIcon className="w-10 text-cream-custom stroke-1" />}
+            />
+            <SidebarIcon
+              icon={
+                <ChatBubbleOvalLeftEllipsisIcon className="w-10 text-cream-custom stroke-1" />
+              }
+            />
+          </div>
+        </div>
+        <div>
+          <img className="w-16" src={logoImg} alt="" />
         </div>
       </div>
     </>
