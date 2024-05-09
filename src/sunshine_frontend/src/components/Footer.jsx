b0b0420217522bf0307fe12f591ec4534e6ebde9
TheoJustin <theojustinx@gmail.com>
2024-05-09 18:34:44 +0700

    added montserrat and polishing stuff

--- src/sunshine_frontend/src/components/Footer.jsx
+++ src/sunshine_frontend/src/components/Footer.jsx
@@ -10,28 +10,28 @@ export default function Footer() {
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <a href={Logo} className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                             <img src={Logo} className="h-16" alt="Sunshine Logo" />
-                            <span className="self-center text-3xl font-semibold whitespace-nowrap text-white">Sunshine</span>
+                            <span className="self-center text-3xl font-semibold whitespace-nowrap text-white font-sans">Sunshine</span>
                         </a>
                         <ul className="flex flex-wrap items-center mb-6 text-lg font-medium text-white sm:mb-0">
                             <li className="mr-6">
-                                <a href="https://discord.com" className="hover:text-blue-300 transition-colors duration-300">
+                                <a href="https://discord.com/users/31709445408948225" className="hover:text-blue-300 transition-colors duration-300">
                                     <FaDiscord size="2em" />
                                 </a>
                             </li>
                             <li className="mr-6">
-                                <a href="https://telegram.org" className="hover:text-sky-300 transition-colors duration-300">
+                                <a href="https://t.me/theojustin" className="hover:text-sky-300 transition-colors duration-300">
                                     <FaTelegramPlane size="2em" />
                                 </a>
                             </li>
                             <li className="mr-6">
-                                <a href="https://instagram.com" className="hover:text-pink-300 transition-colors duration-300">
+                                <a href="https://instagram.com/theojustin_" className="hover:text-pink-300 transition-colors duration-300">
                                     <FaInstagram size="2em" />
                                 </a>
                             </li>
                         </ul>
                     </div>
                     <hr className="my-6 border-gray-300 sm:mx-auto" />
-                    <span className="block text-lg text-white sm:text-center">© 2023 <a href="" className="hover:underline">Sunshine™</a>. All Rights Reserved.</span>
+                    <span className="block text-lg text-white sm:text-center font-sans">© 2023 <a href="" className="hover:underline">Sunshine™</a>. All Rights Reserved.</span>
                 </div>
             </footer>
         </>
--- src/sunshine_frontend/src/pages/AboutPage.jsx
+++ src/sunshine_frontend/src/pages/AboutPage.jsx
@@ -1,23 +1,24 @@
 import { MacbookScroll } from '../components/Macbook';
 import logo from "../../../../assets/motoko.jpg"
 import WavyBackground from "../components/background/WavyBackground";
-import { FaInstagram, FaDiscord } from 'react-icons/fa';
+import { FaTelegramPlane, FaDiscord } from 'react-icons/fa';
 import CountUp from 'react-countup';
+import Footer from '../components/Footer';
 
 function CountingAnimation() {
   return (
     <div className="absolute bottom w-full flex justify-center items-center">
       <div className="flex justify-around items-center w-full max-w-6xl mx-auto">
         <div className="flex flex-col items-center">
-          <div className="text-5xl font-bold mb-2">Users Joined</div>
+          <div className="text-5xl font-bold mb-2 font-sans">Users Joined</div>
           <div className="text-8xl font-bold text-blue-600">
-            <CountUp end={163052} duration={7.5} />
+            <CountUp end={163052} duration={7.5} className='font-sans'/>
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <div className="text-5xl font-bold mb-2">Chats Created</div>
+          <div className="text-5xl font-bold mb-2 font-sans">Chats Created</div>
           <div className="text-8xl font-bold text-purple-600">
-            <CountUp end={54523} duration={7.5} />
+            <CountUp end={54523} duration={7.5} className='font-sans'/>
           </div>
         </div>
       </div>
@@ -39,33 +40,33 @@ function AboutPage() {
         speed="fast"
         waveOpacity={0.6}
       >
-        <div style={{ backgroundColor: '#2EC4B6', padding: '20px', borderRadius: '20px' }} className="container mx-auto py-12 px-4 shadow-lg mb-5">
-          <h1 className="text-3xl font-bold mb-6 text-white">Our Members</h1>
-          <p className="text-lg text-white mb-8">Meet our team members who contributed to this project.</p>
+        <div style={{ backgroundColor: '#2EC4B6', padding: '20px', borderRadius: '20px' }} className="container mx-auto py-12 shadow-lg mb-5">
+          <h1 className="text-3xl font-bold mb-6 text-white font-sans pl-4 pt-4">Our Members</h1>
+          <p className="text-lg text-white mb-8 font-sans pl-4">Meet our team members who contributed to this project.</p>
           
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pl-4 pr-4">
             <div style={{ backgroundColor: '#CBF3F0', borderRadius: '15px' }} className="p-6 shadow-md flex flex-col items-center justify-between">
-              <h2 className="text-2xl font-bold mb-4">Theo Justin Amantha</h2>
-              <p className="text-gray-800 text-sm">Undergraduate Student @Bina Nusantara.</p>
+              <h2 className="text-2xl font-bold mb-4 font-sans">Theo Justin Amantha</h2>
+              <p className="text-gray-800 text-sm font-sans">Undergraduate Student @Bina Nusantara.</p>
               <div className="flex mt-4">
-                <a href="#" className="text-blue-500 hover:text-blue-700"><FaInstagram size={24} /></a>
-                <a href="#" className="text-blue-500 hover:text-blue-700 ml-4"><FaDiscord size={24} /></a>
+                <a href="https://t.me/theojustin" className="text-blue-500 hover:text-blue-700"><FaTelegramPlane size={24} /></a>
+                <a href="https://discord.com/users/31709445408948225" className="text-blue-500 hover:text-blue-700 ml-4"><FaDiscord size={24} /></a>
               </div>
             </div>
             <div style={{ backgroundColor: '#CBF3F0', borderRadius: '15px' }} className="p-6 shadow-md flex flex-col items-center justify-between">
-              <h2 className="text-2xl font-bold mb-4">Jose Jonathan Tano</h2>
-              <p className="text-gray-800 text-sm">Undergraduate Student @Bina Nusantara.</p>
+              <h2 className="text-2xl font-bold mb-4 font-sans">Jose Jonathan Tano</h2>
+              <p className="text-gray-800 text-sm font-sans">Undergraduate Student @Bina Nusantara.</p>
               <div className="flex mt-4">
-                <a href="#" className="text-blue-500 hover:text-blue-700"><FaInstagram size={24} /></a>
-                <a href="#" className="text-blue-500 hover:text-blue-700 ml-4"><FaDiscord size={24} /></a>
+                <a href="https://t.me/yentano" className="text-blue-500 hover:text-blue-700"><FaTelegramPlane size={24} /></a>
+                <a href="https://discord.com/users/295769116403302411" className="text-blue-500 hover:text-blue-700 ml-4"><FaDiscord size={24} /></a>
               </div>
             </div>
             <div style={{ backgroundColor: '#CBF3F0', borderRadius: '15px' }} className="p-6 shadow-md flex flex-col items-center justify-between">
-              <h2 className="text-2xl font-bold mb-4">Ryan Ray Wantouw Oei</h2>
-              <p className="text-gray-800 text-sm">Undergraduate Student @Bina Nusantara.</p>
+              <h2 className="text-2xl font-bold mb-4 font-sans">Ryan Ray Wantouw Oei</h2>
+              <p className="text-gray-800 text-sm font-sans">Undergraduate Student @Bina Nusantara.</p>
               <div className="flex mt-4">
-                <a href="#" className="text-blue-500 hover:text-blue-700"><FaInstagram size={24} /></a>
-                <a href="#" className="text-blue-500 hover:text-blue-700 ml-4"><FaDiscord size={24} /></a>
+                <a href="https://t.me/wantouw" className="text-blue-500 hover:text-blue-700"><FaTelegramPlane size={24} /></a>
+                <a href="https://discord.com/users/693037429908570172" className="text-blue-500 hover:text-blue-700 ml-4"><FaDiscord size={24} /></a>
               </div>
             </div>
           </div>
@@ -80,6 +81,8 @@ function AboutPage() {
           title="Made Using Internet Computer Web 3.0"
           badge=""
         />
+      
+      {/* <Footer/> */}
     </>
   );
 }
